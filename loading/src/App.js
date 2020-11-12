c6d1d1a0095455ef0d43a3e8217dd21fe15eeeb8
devWemillyCristhiny <devwemilly.frontend@gmail.com>
2020-11-12 13:59:59 -0300

    Static: Modificado arquivo e criado componente Loading

--- loading/src/App.js
+++ loading/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Loading from '../src/components/Loading'
 import './App.css';
 
 function App() {
   return (
     <div className="App"> 
-      Trinta dias de css
+      <Loading/>
     </div>
   );
 }
---
+++ loading/src/components/Loading.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Loading() {
+ return (
+   <div className="Container">
+       <span>L</span>
+       <span>O</span>
+       <span>A</span>
+       <span>D</span>
+       <span>I</span>
+       <span>N</span>
+       <span>G</span>
+   </div>
+ );
+}
