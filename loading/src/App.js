ed8dacea66d954d76ed651383379317605b11e7d
devWemillyCristhiny <devwemilly.frontend@gmail.com>
2020-11-12 10:28:13 -0300

    iniciando projeto css

---
+++ loading/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App"> 
+      Trinta dias de css
+    </div>
+  );
+}
+
+export default App;
